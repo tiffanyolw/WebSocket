63aa9bb86bca1599d93e600a093fd9802a88e5ed
twong4 <62853188+twong4@users.noreply.github.com>
2025-02-13 22:14:18 -0500

    Update index.js

--- server_interval/index.js
+++ server_interval/index.js
@@ -5,17 +5,17 @@ const app = express()
 const port = 8080
 
 const wsServer = new ws.Server({ noServer: true });
-var count = 1;
 wsServer.on('connection', socket => {
     wsServer.clients.forEach(function each(client) {
         if (client.readyState === ws.OPEN) {
-            count = 0;
+            var count = 0;
             setInterval(timeout => {
                 count ++;
                 message = { 'source': 'server', 'content': `response from server: ${count}` };
                 client.send(
                     Buffer.from(JSON.stringify(message)),
-                    { binary: false });
+                    { binary: false }
+                );
                 console.log(JSON.stringify(message));
             }, 5000);
         }
